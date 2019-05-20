3bae25f468184a8a52b862833140bd5bf9a9587a
mylene <mylene-desrosier@hotmail.com>
2019-05-20 15:16:35 -0400

    initial commit fix

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to kuku-roller!');
+    expect(page.getTitleText()).toEqual('Welcome to kukuri-roller!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'kuku-roller'`, () => {
+  it(`should have as title 'kukuri-roller'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('kuku-roller');
+    expect(app.title).toEqual('kukuri-roller');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to kuku-roller!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to kukuri-roller!');
   });
 });
